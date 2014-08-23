d0e13eede5ce59f734f87b82b413c20afa2f5674
liquidvapour <liquidvapour@gmail.com>
2014-08-23 17:49:02 +0100

    More code cleaning

--- Source/BinaryFormatViewerCS/GenericArrayReader.cs
+++ Source/BinaryFormatViewerCS/GenericArrayReader.cs
@@ -19,12 +19,7 @@ namespace BinaryFormatViewer
             binaryReader.ReadByte();
             uint numberOfDimentions = binaryReader.ReadUInt32();
 
-            var elementCountPerDimention = new List<uint>();
-
-            foreach (int i in Enumerable.Range(0, (int) numberOfDimentions))
-            {
-                elementCountPerDimention.Add(binaryReader.ReadUInt32());
-            }
+            var elementCountPerDimention = Enumerable.Range(0, (int) numberOfDimentions).Select(i => binaryReader.ReadUInt32()).ToList();
 
             List<TypeSpec> typeSpecs = ReadTypeSpecs(binaryReader, 1U);
 
@@ -32,7 +27,7 @@ namespace BinaryFormatViewer
 
             int nullItemsLeft = 0;
 
-            foreach (int i in Enumerable.Range(0, (int) GetTotalElementCount(elementCountPerDimention)))
+            foreach (int i in Enumerable.Range(0, GetTotalElementCount(elementCountPerDimention)))
             {
                 Node node = null;
                 if (nullItemsLeft == 0)
@@ -63,18 +58,10 @@ namespace BinaryFormatViewer
             return partCode == 7;
         }
 
-        public ulong GetTotalElementCount(List<uint> elementCountPerDimention)
+        private int GetTotalElementCount(IEnumerable<uint> elementCountPerDimention)
         {
-            ulong num = 0UL;
-            using (List<uint>.Enumerator enumerator = elementCountPerDimention.GetEnumerator())
-            {
-                while (enumerator.MoveNext())
-                {
-                    uint current = enumerator.Current;
-                    num += current;
-                }
-            }
-            return num;
+            long result = elementCountPerDimention.Sum(x => (long)x);
+            return checked((int)result);
         }
     }
 }
