abfe660afe64b33c849041a7894050a8a00918d6
liquidvapour <liquidvapour@gmail.com>
2014-12-20 12:40:47 +0000

    Added a TypeCode with values for clarity

---
+++ Source/BinaryFormatViewerCS/TypeCode.cs
@@ -0,0 +1,15 @@
+﻿namespace BinaryFormatViewer
+{
+    public static class TypeCode
+    {
+        public static readonly byte Boolean = 1;
+        public static readonly byte Byte = 2;
+        public static readonly byte Int16 = 7;
+        public static readonly byte Int32 = 8;
+        public static readonly byte Int64 = 9;
+        public static readonly byte DateTime = 13;
+        public static readonly byte UInt32 = 15;
+        public static readonly byte UInt64 = 16;
+
+    }
+}
