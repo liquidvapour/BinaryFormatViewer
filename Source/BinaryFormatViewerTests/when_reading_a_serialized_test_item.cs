fd497496c2e2a54f83295192f57ffe975cf11df9
liquidvapour <liquidvapour@gmail.com>
2014-12-21 15:31:27 +0000

    adding missed file

---
+++ Source/BinaryFormatViewerTests/when_reading_a_serialized_test_item.cs
@@ -0,0 +1,26 @@
+﻿using BinaryFormatViewer;
+using NUnit.Framework;
+
+namespace SerializationSpike
+{
+    [TestFixture]
+    public class when_reading_a_serialized_test_item : BinarySerializedObjectSpec
+    {
+        protected override object GetObjectToSerialize()
+        {
+            return new TestItem("Smith");
+        }
+
+        protected override string GetFileName()
+        {
+            return "lone TestItem.bin";
+        }
+
+        [Test]
+        public void should_have_name_of_TestItem()
+        {
+            var node = (RuntimeObjectNode) result;
+            Assert.AreEqual("SerializationSpike.TestItem", node.Name);
+        }
+    }
+}
